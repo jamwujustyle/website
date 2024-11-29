3d711d7aa99578f145ce1c4dd7c66940bdce4aeb
wind <jamwujustyle@gmail.com>
2024-11-30 02:31:49 +0500

    added flash messages for valid and invalid entries starting with db

--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint("auth", __name__)
 
 
-@auth.route("/login")
+@auth.route("/login", methods=["GET", "POST"])
 def login():
     return render_template("login.html", boolean=True)
 
@@ -13,6 +13,23 @@ def logout():
     return "<p>Logout<p>"
 
 
-@auth.route("/signup")
+@auth.route("/signup", methods=["GET", "POST"])
 def signup():
+    if request.method == "POST":
+        email = request.form.get("email")
+        first_name = request.form.get("firstName")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+
+        if len(email) < 4:
+            flash("email must be greater than 4 characters.", category="error")
+        elif len(first_name) < 2:
+            flash("fiirst name must be greater than 3 characters.", category="error")
+        elif password1 != password2:
+            flash("password dont match.", category="error")
+        elif len(password1) < 5:
+            flash("password must be at least 5 characters.", category="error")
+        else:
+            flash("account created", category="success")
+
     return render_template("signUp.html")
